3a550158960adfc6715d8a82e1a10b5d4cd60e86
mikael <mikael@polaris.ovh>
2024-08-02 15:30:50 +0200

    ubuntu noble kde

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker build --pull --no-cache -t registry.polaris.ovh/docker-webtop:polaris-ubuntu-noble-xfce-latest .
-docker push registry.polaris.ovh/docker-webtop:polaris-ubuntu-noble-xfce-latest
+docker build --pull --no-cache -t registry.polaris.ovh/docker-webtop:polaris-ubuntu-noble-kde-latest .
+docker push registry.polaris.ovh/docker-webtop:polaris-ubuntu-noble-kde-latest
