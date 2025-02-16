2d2ff0a12a849a0efe9ef8ebfe48e400cf68cc19
mikael <mikael@polaris.ovh>
2025-02-16 16:25:40 +0100

    gitea

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker build --pull --no-cache -t registry.polaris.ovh/image-base-webtop:polaris-ubuntu-noble-xfce-latest .
-docker push registry.polaris.ovh/image-base-webtop:polaris-ubuntu-noble-xfce-latest
+docker build --pull --no-cache -t gitea.polaris.ovh/polaris/image-base-webtop:polaris-ubuntu-noble-xfce-latest .
+docker push gitea.polaris.ovh/polaris/image-base-webtop:polaris-ubuntu-noble-xfce-latest
